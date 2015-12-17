867afd5a4fd137e7e30538cdbd911868a7c13ddd
albert.gorski <albert.gorski@motor-talk-gmbh.de>
2015-12-17 19:47:01 +0100

    day 4

---
+++ src/main/scala/albgorski/playground/adventofcode/Day4.scala
@@ -0,0 +1,30 @@
+package albgorski.playground.adventofcode
+
+object Day4 {
+
+  val puzzleInput = "bgvyzdsv"
+
+  val findLowest = (input: String) => {
+    var counter: Int = 0
+    var result: String = ""
+    do {
+      counter = counter + 1
+      result = md5((input + counter).getBytes)
+    } while (!result.startsWith("00000"))
+    counter
+  }
+
+  private[this] def md5(bytes: Array[Byte]): String = {
+    import java.security.MessageDigest
+    val digest = MessageDigest.getInstance("MD5")
+    digest.reset()
+    digest.update(bytes)
+    digest.digest().map(0xFF & _).map {
+      "%02x".format(_)
+    }.foldLeft("") {
+      _ + _
+    }
+  }
+
+
+}
